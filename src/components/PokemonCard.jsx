f3f4642d55dfb991bcf4c37202912c17a6a9866d
FUKUI AKANE <akane.2702@gmail.com>
2023-10-10 17:05:15 +0200

    adding prop-types

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,6 +1,16 @@
+import PropTypes from "prop-types";
+
+
 function PokemonCard ({ pokemon }) {
     /*console.log(props);*/
     console.log(pokemon);
+    PokemonCard.propTypes = {
+        pokemon:PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        imgSrc : PropTypes.string,
+        })
+    }
+    
     return (
         <figure>
             {pokemon.imgSrc === undefined ? <p>???</p> : <img src= {pokemon.imgSrc} alt="" /> }
@@ -9,8 +19,12 @@ function PokemonCard ({ pokemon }) {
             </figcaption>
         </figure>
     )
+    
 }
 export default PokemonCard;
+
+
+
 
      /*   <figure>
             {pokemon.map((poke) => (
