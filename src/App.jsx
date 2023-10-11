1752f38696c2b252fad8e25c0466ea05b97e5de2
FUKUI AKANE <akane.2702@gmail.com>
2023-10-11 10:17:51 +0200

    adding button precedent and suivant

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,60 @@
 import PokemonCard from "./components/PokemonCard"
+import { useState } from "react";
+
 function App(){
   
-  const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-        
-    }
-  ];
+  const [pokemonIndex, setCount] = useState(0);
+
+  const incriment = () => {
+    setCount(pokemonIndex + 1)
+  }
+  
+  const decriment = () => {
+    setCount(pokemonIndex - 1)
+  }
 
   return(
     <div>
-      <PokemonCard pokemon={pokemonList[0]}/>
+      <PokemonCard pokemon={pokemonList[pokemonIndex]}/>
+      {pokemonIndex > 0 
+      ? <button onClick={decriment}>"Précédent"</button> 
+      : <></> }
+
+      {pokemonIndex < pokemonList.length - 1
+      ? <button onClick={incriment}>"Suivant"</button>
+      : <></>
+      }
+     
+      
     </div>
   )
 }
 
 export default App;
 
-
+const pokemonList = [
+  {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
 
